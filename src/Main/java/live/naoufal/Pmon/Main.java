757b0e78768d235cdb9e60ec5c4c61f7ddac7ce7
NaoufalAndichi <00313992@rijnijssel.nl>
2022-04-21 23:32:55 +0200

    text

--- src/Main/java/live/naoufal/Pmon/Main.java
+++ src/Main/java/live/naoufal/Pmon/Main.java
@@ -34,20 +34,53 @@ public class Main {
     //arraylist
     static List<String> containerNames = new ArrayList<>();
 
+    public static Boolean healthy = false;
 
     public static void main(String[] args) {
         Runnable helloRunnable = new Runnable() {
+            private int previous;
+            WebhookClientBuilder builder = new WebhookClientBuilder("https://discord.com/api/webhooks/951127407786614834/gUx-8ElAjs4JhEMmiaAaIphJSlDyh1f_t4BEGp5W1srJoyFME1p1xAXrQ0nUomnp-mej");
+            WebhookClient webhookClient = WebhookClient.withUrl("https://discord.com/api/webhooks/951127407786614834/gUx-8ElAjs4JhEMmiaAaIphJSlDyh1f_t4BEGp5W1srJoyFME1p1xAXrQ0nUomnp-mej");
+
+            private List healthyContainers() {
+                builder.setWait(true);
+                for (Container container : dockerClient.listContainersCmd().exec()) {
+//                    webhookClient.send(String.join(", ", container.getNames()));
+                }
+
+                ArrayList<String> containers = new ArrayList<>();
+                for (Container container : Main.dockerClient.listContainersCmd().exec()) {
+                    System.out.println(container.getState());
+                    if ("running".equals(container.getState())) {
+//                        System.out.println(String.join(", ", container.getNames()));
+                        containers.add(String.join(", ", container.getNames()));
+                    }
+                }
+                return containers;
+            }
+
             @Override
             public void run() {
-                webhookClient.send("**Message**");
-
-                WebhookEmbed embed = new WebhookEmbedBuilder()
-                        .setColor(65280)
-                        .setDescription("healthy :white_check_mark:")
-                        .setImageUrl("https://media.istockphoto.com/vectors/green-check-mark-icon-green-tick-symbol-round-checkmark-sign-vector-vector-id1159270056?k=20&m=1159270056&s=170667a&w=0&h=ewlZtL_NAF5L4dFRHxWLmNpZtnWyvxtDQ6BEPVrvlzw=")
-                        .build();
-                webhookClient.send(embed)
-                        .thenAccept((message) -> System.out.printf("Message with embed has been sent [%s]%n", message.getId()));
+//                webhookClient.send("**Message**");
+                String description;
+                String imageUrl;
+                if (healthyContainers().size() >= 1) {
+                    description = healthyContainers() + "healthy :white_check_mark:";
+                    imageUrl = "https://media.istockphoto.com/vectors/green-check-mark-icon-green-tick-symbol-round-checkmark-sign-vector-vector-id1159270056?k=20&m=1159270056&s=170667a&w=0&h=ewlZtL_NAF5L4dFRHxWLmNpZtnWyvxtDQ6BEPVrvlzw=";
+                } else {
+                    description = "unhealthy :x:";
+                    imageUrl = "https://media.istockphoto.com/vectors/design-of-red-wrong-mark-grunge-letter-xred-cross-sign-hand-drawn-vector-id1214857021?k=20&m=1214857021&s=612x612&w=0&h=vMllYNYlBX5rHw-5r0MS0gROSXokOcbQmGtvhVOWVEI=";
+                }
+                if (previous != healthyContainers().size()) {
+                    WebhookEmbed embed = new WebhookEmbedBuilder()
+                            .setColor(65280)
+                            .setDescription(description)
+                            .setImageUrl(imageUrl)
+                            .build();
+                    webhookClient.send(embed)
+                            .thenAccept((message) -> System.out.printf("Message with embed has been sent [%s]%n", message.getId()));
+                }
+                previous = healthyContainers().size();
             }
         };
         ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
